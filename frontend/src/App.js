4cef819bf75fcbf040afc0a7ddc6489f384ee439
Alliyghan <aliygan.itwork@gmail.com>
2022-07-17 16:02:30 +0700

    Video-03-Create-React-Apps

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
